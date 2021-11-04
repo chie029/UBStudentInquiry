0d8343924fca974740639f8d1e0214c94ce59870
Chie029 <jhachie029@gmail.com>
2021-11-04 17:44:20 +0800

    commit

--- Resource.php
+++ Resource.php
@@ -5,22 +5,23 @@ class Resource {
     function getAllManager(){
 
         $jsonUsers = array();
-        $getUsers = fopen("Users/Manager/Users.txt", "r") or die("Unable to open file!");
-        if (filesize("Users/MANAGER/Users.txt") > 0) {
-            $string = fread($getUsers, filesize("Users/MANAGER/Users.txt"));
-            $jsonUsers = json_decode($string);
-            fclose($getUsers);
-            array_push($jsonUsers, "MANAGER");
-            $json = [
-                'status' => '200',
-                'response' => json_encode($jsonUsers)
-            ];
-        } else {
-            array_push($jsonUsers, "MANAGER");
-            $json = [
-                'status' => '200',
-                'response' => json_encode($jsonUsers)
-            ];
+        if ($getUsers = fopen("Users/Manager/Users.txt", "r")) {
+            if (filesize("Users/MANAGER/Users.txt") > 0) {
+                $string = fread($getUsers, filesize("Users/MANAGER/Users.txt"));
+                $jsonUsers = json_decode($string);
+                fclose($getUsers);
+                array_push($jsonUsers, "MANAGER");
+                $json = [
+                    'status' => '200',
+                    'response' => json_encode($jsonUsers)
+                ];
+            } else {
+                array_push($jsonUsers, "MANAGER");
+                $json = [
+                    'status' => '200',
+                    'response' => json_encode($jsonUsers)
+                ];
+            }
         }
 
         return json_encode($json);
